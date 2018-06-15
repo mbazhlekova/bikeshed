dc6679c04db57119104cd954b7f5d037196c07f2
Maria B <mbazhlek@gmail.com>
2018-06-15 12:19:57 +1000

    Add file

---
+++ client/webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const dev =
+  process.env.NODE_ENV !== 'production' && process.argv.indexOf('-p') === -1;
+
+const HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: path.join(__dirname, '/src/index.html'),
+  favicon: 'favicon.png',
+  filename: 'index.html',
+  inject: 'body',
+});
+
+const DefinePluginConfig = new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify('production'),
+});
+
+const UglifyJsPluginConfig = new webpack.optimize.UglifyJsPlugin({
+  beautify: false,
+  mangle: {
+    screw_ie8: true,
+  },
+  compress: {
+    screw_ie8: true,
+  },
+  comments: false,
+});
+
+module.exports = {
+  devServer: {
+    host: 'localhost',
+    port: '3000',
+    hot: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    proxy: {
+      '/api/**': 'http://localhost:5000',
+      '/auth/github': 'http://localhost:5000',
+    },
+    historyApiFallback: true,
+    disableHostCheck: true,
+  },
+  entry: [
+    'react-hot-loader/patch',
+    'babel-polyfill',
+    path.join(__dirname, '/src/index.jsx'),
+  ],
+  module: {
+    loaders: [{
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, '/build'),
+    publicPath: '/',
+  },
+  plugins: dev ?
+    [
+      HTMLWebpackPluginConfig,
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+    ] :
+    [HTMLWebpackPluginConfig, DefinePluginConfig, UglifyJsPluginConfig],
+};
